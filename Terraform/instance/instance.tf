99b63f395d525462ea253b9a2cf872470271a7bf
acoyp99 <61330297+acoyp99@users.noreply.github.com>
2020-11-30 12:08:07 -0500

    Update instance.tf

--- Terraform/instance/instance.tf
+++ Terraform/instance/instance.tf
@@ -53,10 +53,25 @@ resource "aws_instance" "web" {
 
     tags                        = {
         Name                    = "sim-FrontEnd"
-    }
-    
-    
+    }   
+}
 
-    
+resource "aws_instance" "api2" {
+    ami                         = data.aws_ami.ubuntu.id
+    instance_type               = "t2.micro"
+    key_name                    = "sim-key"
+    subnet_id                   = var.subnet_id
+    vpc_security_group_ids      = var.vpc_security_group_ids
+    user_data = <<-EOF
+        #!/bin/bash
+        # install docker
+        sudo curl -sSL https://get.docker.com/ | sh
+        # run dummy service that returns host id and load info
+        sudo docker run --detach --publish 8000:8000 camandel/django-wiki
+        EOF 
+
+    tags                        = {
+        Name                    = "sim-BackEnd2"
+    }
 }
 
